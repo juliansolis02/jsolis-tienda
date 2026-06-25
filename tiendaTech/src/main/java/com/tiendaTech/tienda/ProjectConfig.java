f3d7284d309afc342d481a1315ce9cce854bb2ca
José Julián Solís <julianmurillo2k05@gmail.com>
2026-06-25 15:23:45 -0600

    commit semana 5
    Elaborado por José Julián Solís Murillo.

--- tiendaTech/src/main/java/com/tiendaTech/tienda/ProjectConfig.java
+++ tiendaTech/src/main/java/com/tiendaTech/tienda/ProjectConfig.java
@@ -1,3 +1,4 @@
+
 package com.tiendaTech.tienda;
 
 import java.util.Locale;
---
+++ tiendaTech/src/main/java/com/tiendaTech/tienda/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.tiendaTech.tienda.repository;
+
+import com.tiendaTech.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+    public List<Producto> findByActivoTrue();
+}
